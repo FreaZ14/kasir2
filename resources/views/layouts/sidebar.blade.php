f1da21a4dbf8a7104db848609413e4de5abd53e0
Farhan <syauqifarhan211@gmail.com>
2025-03-17 13:59:44 +0700

    done?

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -11,43 +11,31 @@
 
         <li class="nav-heading">Pages</li>
 
-        {{-- <li class="nav-item">
-            <a class="nav-link {{ request()->is('/contoh-1') ? 'active' : 'collapsed' }}" href="/contoh-1">
-                <i class="bi bi-person"></i>
-                <span>Profile</span>
-            </a>
-        </li><!-- End Profile Page Nav --> --}}
+        @if (auth()->user()->id != 2)
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('users*') ? 'active' : 'collapsed' }}"
+                    href="{{ route('users.index') }}">
+                    <i class="bi bi-people"></i>
+                    <span>Users</span>
+                </a>
+            </li>
 
-        {{-- <li class="nav-item">
-            <a class="nav-link {{ request()->is('/contoh-2') ? 'active' : 'collapsed' }}" href="/contoh-2">
-                <i class="bi bi-question-circle"></i>
-                <span>F.A.Q</span>
-            </a>
-        </li><!-- End F.A.Q Page Nav --> --}}
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('barang*') ? 'active' : 'collapsed' }}"
+                    href="{{ route('barang.index') }}">
+                    <i class="bi bi-box"></i>
+                    <span>Barang</span>
+                </a>
+            </li>
 
-        <li class="nav-item">
-            <a class="nav-link {{ request()->routeIs('users*') ? 'active' : 'collapsed' }}"
-                href="{{ route('users.index') }}">
-                <i class="bi bi-people"></i>
-                <span>Users</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link {{ request()->routeIs('barang*') ? 'active' : 'collapsed' }}"
-                href="{{ route('barang.index') }}">
-                <i class="bi bi-box"></i>
-                <span>Barang</span>
-            </a>
-        </li>
-
-        <li class="nav-item">
-            <a class="nav-link {{ request()->routeIs('pembelian*') ? 'active' : 'collapsed' }}"
-                href="{{ route('pembelian.index') }}">
-                <i class="bi bi-basket2"></i>
-                <span>Pembelian</span>
-            </a>
-        </li>
+            <li class="nav-item">
+                <a class="nav-link {{ request()->routeIs('pembelian*') ? 'active' : 'collapsed' }}"
+                    href="{{ route('pembelian.index') }}">
+                    <i class="bi bi-basket2"></i>
+                    <span>Pembelian</span>
+                </a>
+            </li>
+        @endif
 
         <li class="nav-item">
             <a class="nav-link {{ request()->routeIs('penjualan*') ? 'active' : 'collapsed' }}"
